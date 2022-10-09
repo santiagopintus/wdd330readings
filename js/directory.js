0d78f798a1a9f839687accb818b8235262f2898a
Santiago Pintus <santiagopintus@gmail.com>
2022-10-08 21:02:17 -0300

    Added W04 readings

--- js/directory.js
+++ js/directory.js
@@ -13,5 +13,10 @@ export const links = [
     label: "Week 03 notes",
     navLabel: "Week 03",
     url: "pages/w03",
+  },
+  {
+    label: "Week 04 notes",
+    navLabel: "Week 04",
+    url: "pages/w04",
   }
 ];
